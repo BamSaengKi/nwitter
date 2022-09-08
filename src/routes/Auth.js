923cae9fd097212d1232a457da65d014fc25a2f8
JInwoo Oh <jinwoooh@JInwooui-MacBookPro.local>
2022-09-08 11:36:53 +0900

    [UPDATE] Log In

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,9 +1,13 @@
+import { authService } from "fbase";
 import React, { useState } from "react";
 
 const Auth = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("")
+    const [newAccount, setNewAccount] = useState(true)
+    const [error, setError] = useState("")
     const onChange = (event) => {
+
         const { target: { name, value } } = event;
         if (name === "email") {
             setEmail(value);
@@ -11,21 +15,53 @@ const Auth = () => {
             setPassword(value);
         }
     };
-    const onSubmit = (event) => {
-        event.preventDefault();
+    const onSubmit = async (event) => {
+        event.preventDefault();   // preventDefault : 디폴트 행위가 실행되지 않게 해줌 //
+        try {
+            let data;
+            if (newAccount) {
+                data = await authService.createUserWithEmailAndPassword(
+                    email, password)
+            } else {
+                data = await authService.signInWithEmailAndPassword(
+                    email, password)
+            }
+            console.log(data)
+        } catch (error) {
+            setError(error.message)
+
+        }
     };
+
+    const toggleAccount = ()=> setNewAccount((prev)=> !prev)
     return (
         <div>
             <form onSubmit={onSubmit}>
-                    <input name="email" type="text" placeholder="Email" required value={email} onChange={onChange} />
-                    <input name="password" type="password" placeholder="Password" required value={password} onChange={onChange} />
-                    <input type="submit" value="Log in" />
+
+                <input
+                    name="email"
+                    type="text"
+                    placeholder="Email"
+                    required value={email}
+                    onChange={onChange}
+                />
+                <input
+                    name="password"
+                    type="password"
+                    placeholder="Password"
+                    required value={password}
+                    onChange={onChange}
+                />
+                <input type="submit" value={newAccount ? "Create Account" : "Sign  In"} />
+                {error}
+                </form>
+                <span onClick={toggleAccount}>{newAccount ? "Sign In" : "Create Account"}</span>
                 <div>
                     <button>Continue with Google</button>
                     <button>Continue with Github</button>
                 </div>
-            </form>
+            
         </div>
     )
-}
+} 
 export default Auth;
