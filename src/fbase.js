e68bcf601eb21d01f07367784d89e558eddbc394
JInwoo Oh <jinwoooh@JInwooui-MacBookPro.local>
2022-09-08 16:31:50 +0900

    [UPDATE] Tweet Form

--- src/fbase.js
+++ src/fbase.js
@@ -3,7 +3,7 @@ import "firebase/compat/auth";
 import "firebase/compat/firestore";
 import "firebase/compat/storage";
 import "firebase/auth";
-
+import "firebase/databse"
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -12,9 +12,11 @@ const firebaseConfig = {
   projectId: process.env.REACT_PROJECT_ID,
   storageBucket: process.env.REACT_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_MESSAGEIN_ID,
-  appId: process.env.REACT_APP_APP_ID
+  appId: process.env.REACT_APP_APP_ID,
 };
 
-firebase.initializeApp(firebaseConfig)
+firebase.initializeApp(firebaseConfig);
+
+export const firebaseInstance = firebase;
 
 export const authService = firebase.auth();
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,3 +1,19 @@
+import { authService } from "fbase";
 import React from "react";
+import { useHistory } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
 
-export default () => <span>Profile</span>
+export default () => {
+  // const navigation = useNavigate();
+  const history = useHistory();
+  const onLogOutClick = () => {
+    authService.signOut();
+    history.push("/")
+  };
+
+  return (
+    <>
+      <button onClick={onLogOutClick}>Sign Out</button>
+    </>
+  );
+};
